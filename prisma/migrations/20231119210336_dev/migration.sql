844050e00ae6a41a8509c62363fb7986c856f2ef
German <german@MacBook-Air-German.local>
2023-11-20 00:04:01 +0300

    feat: add user schema and new migration

---
+++ prisma/migrations/20231119210336_dev/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "first_name" VARCHAR(100) NOT NULL,
+    "last_name" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "email_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "img_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("_id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_id_key" ON "profiles"("id");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_id_fkey" FOREIGN KEY ("id") REFERENCES "users"("_id") ON DELETE RESTRICT ON UPDATE CASCADE;
