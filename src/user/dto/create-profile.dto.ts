40fb60a3efda41ecc8e59ce554a2bd7199f72e3e
German <german@MacBook-Air-German.local>
2023-12-17 13:52:33 +0300

    ref: dto profile

--- src/user/dto/create-profile.dto.ts
+++ src/user/dto/create-profile.dto.ts
@@ -1,21 +1,4 @@
 import { IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
 
 export class CreateProfileDto {
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(100)
-    firstName: string
-
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(100)
-    lastName: string
-
-    @IsString()
-    @IsOptional()
-    imgUrl?: string
-
-    @IsString()
-    @IsOptional()
-    key?: string
 }
